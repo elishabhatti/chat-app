e52b7b188113f90a4721b674f79f8d7cf3705c75
elisha <elishajameel2007@gmail.com>
2025-10-01 15:03:30 +0500

    added port from .env

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,6 +1,7 @@
 import express from "express";
 import authRoutes from "../routes/auth.routes.js";
 import dotenv from "dotenv"
+import { connectDB } from "../lib/db.js";
 dotenv.config()
 
 const app = express();
@@ -10,4 +11,5 @@ app.use("/api/auth", authRoutes);
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
+  connectDB()
 });
