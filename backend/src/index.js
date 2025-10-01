cc11acbd536ae1ad0a9e295df152fb99e8d8c21d
elisha <elishajameel2007@gmail.com>
2025-10-01 14:55:00 +0500

    enable dotenv

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,5 +1,7 @@
 import express from "express";
 import authRoutes from "../routes/auth.routes.js";
+import dotenv from "dotenv"
+dotenv.config()
 
 const app = express();
 const PORT = process.env.PORT
@@ -7,5 +9,5 @@ const PORT = process.env.PORT
 app.use("/api/auth", authRoutes);
 
 app.listen(PORT, () => {
-  console.log("Server is running on port 5001");
+  console.log(`Server is running on port ${PORT}`);
 });
