7a61afcaa86e7c488ec722fdf4d7167914b4e320
elisha <elishajameel2007@gmail.com>
2025-10-01 14:54:07 +0500

    added port in env

--- backend/src/index.js
+++ backend/src/index.js
@@ -2,9 +2,10 @@ import express from "express";
 import authRoutes from "../routes/auth.routes.js";
 
 const app = express();
+const PORT = process.env.PORT
 
 app.use("/api/auth", authRoutes);
 
-app.listen(5001, () => {
+app.listen(PORT, () => {
   console.log("Server is running on port 5001");
 });
