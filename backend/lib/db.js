7e5212239ae6a231873524d3883656564e82fe3f
elisha <elishajameel2007@gmail.com>
2025-10-02 00:12:39 +0500

    removed unwanted code form connection db file

--- backend/lib/db.js
+++ backend/lib/db.js
@@ -2,8 +2,8 @@ import mongoose from "mongoose";
 
 export const connectDB = async () => {
   try {
-    const conn = await mongoose.connect(process.env.MONGODB_URI);
-    console.log(`MongoDB Connected ${conn.connection.host}`);
+    await mongoose.connect(process.env.MONGODB_URI);
+    console.log(`MongoDB Connected`);
   } catch (error) {
     console.error(`MongoDB connection error: ${error}`);
   }
