f1730d1cc2ab162f4746711f6036667d99d96cf9
elisha <elishajameel2007@gmail.com>
2025-10-01 14:45:04 +0500

    send message for signup

--- backend/routes/auth.routes.js
+++ backend/routes/auth.routes.js
@@ -1,6 +1,10 @@
 import { Router } from "express";
 
 const route = Router();
-
+route.get("/signup", (req, res) => {
+  res.send("route signup");
+});
+// route.get("/login")
+// route.get("/logout")
 
 export default route;
