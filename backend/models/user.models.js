8d361bd8f4598ad14d82bc7c4ecf7b65c108ce5f
elisha <elishajameel2007@gmail.com>
2025-10-02 01:16:14 +0500

    addded new keywork in models

--- backend/models/user.models.js
+++ backend/models/user.models.js
@@ -1,6 +1,6 @@
 import mongoose from "mongoose";
 
-const userSchema = mongoose.Schema(
+const userSchema = new mongoose.Schema(
   {
     email: { type: String, required: true, unique: true },
     fullName: { type: String, required: true },
