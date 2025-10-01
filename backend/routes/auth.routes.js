7fa9e39a4349f824e68410e71cb57cc2945a9c7c
elisha <elishajameel2007@gmail.com>
2025-10-02 00:29:28 +0500

    fix bug added post in login controller

--- backend/routes/auth.routes.js
+++ backend/routes/auth.routes.js
@@ -4,7 +4,7 @@ import { login, logout, signUp } from "../controllers/auth.controller.js";
 const route = Router();
 
 route.post("/signup", signUp);
-route.get("/login", login);
+route.post("/login", login);
 route.get("/logout", logout);
 
 export default route;
