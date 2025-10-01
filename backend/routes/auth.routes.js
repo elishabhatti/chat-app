22c7fc495e3d02b8f507973bf117f1b03dd9ce79
elisha <elishajameel2007@gmail.com>
2025-10-01 14:53:11 +0500

    added .env sample file

--- backend/routes/auth.routes.js
+++ backend/routes/auth.routes.js
@@ -2,6 +2,7 @@ import { Router } from "express";
 import { login, logout, signUp } from "../controllers/auth.controller.js";
 
 const route = Router();
+
 route.post("/signup", signUp);
 route.get("/login", login);
 route.get("/logout", logout);
