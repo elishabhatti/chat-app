e30df9062582da4fa796777dff7c5d1297117bff
elisha <elishajameel2007@gmail.com>
2025-10-02 19:15:56 +0500

    added data for home page

---
+++ frontend/src/store/useChatStore.js
@@ -0,0 +1,37 @@
+import { create } from "zustand";
+import toast from "react-hot-toast";
+import { axiosInstance } from "../lib/axios";
+
+export const useChatStore = create((set) => ({
+  message: [],
+  users: [],
+  selectedUser: null,
+  isUserLoading: false,
+  isMessagesLoading: false,
+
+  getUsers: async () => {
+    set({ isUserLoading: true });
+    try {
+      const res = await axiosInstance.get("/messages/users");
+      set({ users: res.data });
+    } catch (error) {
+      toast.error(error);
+    } finally {
+      set({ isUserLoading: false });
+    }
+  },
+
+  getMessages: async (userId) => {
+    set({ isMessagesLoading: true });
+    try {
+      const res = await axiosInstance.get(`/messages/${userId}`);
+      set({ message: res.data });
+    } catch (error) {
+      toast.error(error);
+    } finally {
+      set({ isMessagesLoading: false });
+    }
+  },
+  // todo: optimize this one later
+  setSelectedUser: async (selectedUser) => set({ selectedUser }),
+}));
